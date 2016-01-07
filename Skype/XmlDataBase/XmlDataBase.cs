dc21322814f1cec92ab82811b0e5d33e4713604e
Cobuz Veniamin <ronaldo07min@gmail.com>
2016-01-07 17:42:52 +0200

    Log in + Register + Delete User + Add Friend

--- Skype/XmlDataBase/XmlDataBase.cs
+++ Skype/XmlDataBase/XmlDataBase.cs
@@ -10,19 +10,22 @@ namespace Interogare
 {
     public class XmlDataBase
     {
-        public int LogIn(string username, string parola)
+        public int verifyUser(string username, string parola)
         {
             int count = 0;
-
-            XElement root = XElement.Load("DB.xml");
+            XElement xDoc = XElement.Load("DB.xml");
             IEnumerable<XElement> address =
-                from el in root.Elements("users").Elements("user")
+                from el in xDoc.Elements("users").Elements("user")
                 where (string)el.Attribute("username") == username & (string)el.Element("parola") == parola
                 select el;
             foreach (XElement el in address)
                 count++;
+            return count;
+        }
 
-            if (count == 0)
+        public int LogIn(string username, string parola)
+        {
+            if (verifyUser(username, parola) == 0)
             {
                 //Console.WriteLine("Username-ul sau parola sunt gresite");
                 return 0;
@@ -34,16 +37,28 @@ namespace Interogare
             }
         }
 
-        public void Delete(string username)
+        public int Delete(string username, string parola)
         {
-            XmlDocument doc = new XmlDocument();
-            doc.Load("DB.xml");
-            XmlNodeList nodes = doc.SelectNodes("//user[@username='" + username + "']");
-            for (int i = nodes.Count - 1; i >= 0; i--)
+            if (verifyUser(username, parola) == 0)
             {
-                nodes[i].ParentNode.RemoveChild(nodes[i]);
+                //Console.WriteLine("Nu ai dreptul sa stergi acest cont");
+                return 0;
             }
-            doc.Save("DB.xml");
+            else
+            {
+
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load("DB.xml");
+                XmlNodeList nodes = xDoc.SelectNodes("//user[@username='" + username + "']");
+                for (int i = nodes.Count - 1; i >= 0; i--)
+                {
+                    nodes[i].ParentNode.RemoveChild(nodes[i]);
+                }
+                xDoc.Save("DB.xml");
+
+                //Console.WriteLine("Cont sters!");
+                return 1;
+            }
         }
 
         public int Register(string username, string parola, string reParola, string nume, string prenume, string varsta)
@@ -55,9 +70,16 @@ namespace Interogare
 
             int count = 0;
 
-            XElement root = XElement.Load("DB.xml");
+            XElement xDoc = XElement.Load("DB.xml");
+
+            if (xDoc == null)
+            {
+                //Console.WriteLine("S-a produs o eroare la incarcarea xml-ului");
+                return 0;
+            }
+
             IEnumerable<XElement> address =
-                from el in root.Elements("user")
+                from el in xDoc.Elements("users").Elements("user")
                 where (string)el.Attribute("username") == username
                 select el;
 
@@ -66,24 +88,19 @@ namespace Interogare
 
             if (count == 0)
             {
-                var xDoc = XElement.Load("DB.xml");
-                if (xDoc == null)
-                {
-                    Console.WriteLine("S-a produs o eroare la incarcarea xml-ului");
-                    return 0;
-                }
-
                 var myNewElement = new XElement("user",
-
                    new XAttribute("username", username),
                    new XElement("parola", parola),
                    new XElement("nume", nume),
                    new XElement("prenume", prenume),
-                   new XElement("varsta", varsta),
-                   new XElement("prieteni", null)
+                   new XElement("varsta", varsta)
                     //And so on ...
                 );
-                xDoc.Add(myNewElement);
+
+                var myNewElement2 = new XElement(username);
+
+                xDoc.Element("users").Add(myNewElement);
+                xDoc.Element("friends").Add(myNewElement2);
                 xDoc.Save("DB.xml");
                 //Console.WriteLine("Inregistrare reusita");
                 return 1;
@@ -93,6 +110,19 @@ namespace Interogare
                 //Console.WriteLine("Username deja folosit");
                 return 0;
             }
+        }
+
+        public int AddFriend(string username, string friend)
+        {
+            var myNewElement = new XElement("friend", "ion");
+
+            XElement xDoc = XElement.Load("DB.xml");
+            if (xDoc == null)
+                //Console.WriteLine("S-a produs o eroare la incarcarea xml-ului");
+                return 0;
+            xDoc.Element("friends").Element(username).Add(myNewElement);
+            xDoc.Save("DB.xml");
+            return 1;
         }
     }
 }
